8e1dcbb1e3d59fedc312d4f31b08cf87616ff3f8
Arslan Ahmad <arslan.viralsquare@gmail.com>
2022-10-11 22:10:21 +0500

    show user on dashboard

--- app/controllers/admins/dashboard_controller.rb
+++ app/controllers/admins/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Admins::DashboardController < ApplicationController
   before_action :authenticate_admin!
   def index
-
+    @users = User.paginate(page: params[:page], per_page: 4).order('created_at desc')
+    @male = User.count_male_user
+    @female = User.count_female_user
   end
 end
