b493330954b757b9ca264b6c82b044e9061cfe1d
alihussain47 <mali1995ror@gmail.com>
2022-10-21 23:01:03 +0500

    jbuilder added

--- app/controllers/api/v1/media_controller.rb
+++ app/controllers/api/v1/media_controller.rb
@@ -4,13 +4,15 @@ class Api::V1::MediaController < Api::V1::ApiController
   def update_media
     # @titles = media_params[:title].map{ |title| title.downcase }
     @media = @current_user.social_media.where(title: media_params[:title])
-    @media.update(link: media_params[:link])
+    @media.each_with_index do |media, index|
+      media.update(link: media_params[:link][index])
+    end
   end
 
   private
 
   def media_params
-    params.require(:media).permit(title: [], link: [])
+    params.require(:media).permit(link: [], title: [])
     
   end
 end
