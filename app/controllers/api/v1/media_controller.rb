50de658f41cd67c2b982fa06ab26769f82df58fb
alihussain47 <mali1995ror@gmail.com>
2022-10-24 13:20:04 +0500

    update_media api updated

--- app/controllers/api/v1/media_controller.rb
+++ app/controllers/api/v1/media_controller.rb
@@ -2,8 +2,9 @@ class Api::V1::MediaController < Api::V1::ApiController
   before_action :authorize_user
 
   def update_media
-    # @titles = media_params[:title].map{ |title| title.downcase }
-    @media = @current_user.social_media.where(title: media_params[:title])
+    @titles = media_params[:title].map{ |title| title.downcase }
+    @media = @current_user.social_media.where('title IN (?)',  @titles)
+    
     @media.each_with_index do |media, index|
       media.update(link: media_params[:link][index])
     end
