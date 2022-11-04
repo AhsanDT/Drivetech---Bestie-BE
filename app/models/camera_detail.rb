f002082fdd42d8bfe6bddc98f47b2faa8d1036fe
Awais Manzoor <awais.manzoor@viralsquare.org>
2022-11-04 18:47:04 +0500

    Created enum for camera_type

--- app/models/camera_detail.rb
+++ app/models/camera_detail.rb
@@ -1,3 +1,12 @@
 class CameraDetail < ApplicationRecord
   belongs_to :user
+
+  enum camera_type: {
+    "iPhone" => 0,
+    "Digital Camera" => 1,
+    "Film Camera" => 2,
+    "8mm" => 3,
+    "Polaroid" => 4,
+    "Camcorder" => 5
+  }
 end
