50883790dcd4ce7aeace647f34124de43df23aef
Awais Manzoor <awais.manzoor@viralsquare.org>
2022-11-14 19:27:09 +0500

    notification implemnted with fcm

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,9 @@
 class Notification < ApplicationRecord
   belongs_to :user
+
+  after_create :call_notification_service
+
+  def call_notification_service
+    FcmNotificationsService.push_notification(self)
+  end
 end
