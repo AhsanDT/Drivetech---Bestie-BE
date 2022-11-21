3695b995078bedf0d02a3e4485c18bbea14dccd1
Awais Manzoor <awais.manzoor@viralsquare.org>
2022-11-21 17:48:36 +0500

    changed action cable url in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,12 +120,12 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_cable.url = 'wss://bestie-staging.herokuapp.com/cable'
+  config.action_cable.url = 'wss://bestie-b.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = {  host: 'https://bestie-staging.herokuapp.com/' }
+  config.action_mailer.default_url_options = {  host: 'https://bestie-b.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
