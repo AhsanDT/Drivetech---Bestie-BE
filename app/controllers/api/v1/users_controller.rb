9a571be048a749ff499adf82db585ecad922f6d7
Awais Manzoor <awais.manzoor@viralsquare.org>
2022-11-21 20:04:18 +0500

    refactoring in users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,20 @@
 class Api::V1::UsersController < Api::V1::ApiController
   before_action :authorize_user
+  before_action :find_nearby_users, only: [:suggested_besties, :besties_near_you]
 
   def suggested_besties
     @besties = User.where.not(id: @current_user.id).joins(:subscriptions).uniq
-    @nearby_users = User.in_range(1..1000, :units => :miles, :origin => [@current_user.latitude, @current_user.longitude])
     @besties = (@besties & @nearby_users)
   end
 
   def besties_near_you
     @besties = User.where.missing(:subscriptions)
-    @nearby_users = User.in_range(1..1000, :units => :miles, :origin => [@current_user.latitude, @current_user.longitude])
     @besties = (@besties & @nearby_users)
+  end
+
+  private
+
+  def find_nearby_users
+    @nearby_users = User.in_range(1..1000, :units => :miles, :origin => [@current_user.latitude, @current_user.longitude])
   end
 end
