a1eb3cc0f9ab79c58aca3dcc6b00ff52faabd712
Awais Manzoor <awais.manzoor@viralsquare.org>
2022-11-21 17:45:23 +0500

    changes in suggested users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,13 +3,15 @@ class Api::V1::UsersController < Api::V1::ApiController
 
   def suggested_besties
     @subscription_users = User.where.not(id: @current_user.id).joins(:subscriptions).uniq
+    @nearby_users = User.in_range(1..1000, :units => :miles, :origin => [@current_user.latitude, @current_user.longitude])
+    @subscription_users = (@subscription_users & @nearby_users)
     render json: {data: @subscription_users}
   end
 
   def besties_near_you
     @besties = User.where.missing(:subscriptions)
-    @nearby_users = User.within(1,:units => :kms,:origin => [@current_user.latitude, @current_user.longitude])
-    @besties.union(@nearby_users)
-    render json: {data: @nearby_users}
+    @nearby_users = User.in_range(1..1000, :units => :miles, :origin => [@current_user.latitude, @current_user.longitude])
+    @besties = (@besties & @nearby_users)
+    render json: {data: @besties}
   end
 end
