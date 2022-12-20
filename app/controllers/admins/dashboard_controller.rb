baf3210e8c0fecf884765001d899ffac802c746a
Awais Manzoor <awais.manzoor@viralsquare.org>
2022-12-20 19:30:09 +0500

    subscriptions on dashboard

--- app/controllers/admins/dashboard_controller.rb
+++ app/controllers/admins/dashboard_controller.rb
@@ -4,6 +4,8 @@ class Admins::DashboardController < ApplicationController
     @users = User.paginate(page: params[:page], per_page: 4).order('created_at desc')
     @male = User.count_male_user
     @female = User.count_female_user
-    @subscriptions = Subscription.all.count
+    @gold_subscriptions = Package.joins(:subscriptions).where(name: "Gold Package").count
+    @silver_subscriptions = Package.joins(:subscriptions).where(name: "Silver Package").count
+    @bronze_subscriptions = Package.joins(:subscriptions).where(name: "Bronze Package").count
   end
 end
