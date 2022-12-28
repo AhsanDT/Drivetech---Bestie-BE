4c17ad11c5a5086d26b27976dbd481a3f6bf5068
Awais Manzoor <awais.manzoor@viralsquare.org>
2022-12-28 13:51:46 +0500

    reset password issue resolved

--- app/controllers/admins/passwords_controller.rb
+++ app/controllers/admins/passwords_controller.rb
@@ -3,6 +3,15 @@ class Admins::PasswordsController < Devise::PasswordsController
   def custom_reset_password_action
   end
 
+  def create
+    @admin = Admin.find_by(email: params[:admin][:email])
+    if @admin.present?
+      super
+    else
+      redirect_to new_admin_password_path
+    end
+  end
+
   protected
   def after_sending_reset_password_instructions_path_for(resource_name)
     reset_password_instruction_path
