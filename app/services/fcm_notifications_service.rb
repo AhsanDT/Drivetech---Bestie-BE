5fffa6e56b81fe8a4eeb42ce62da1affb40b2b16
Awais Manzoor <awais.manzoor@viralsquare.org>
2023-01-10 14:16:11 +0500

    conversation id in current user notifications

--- app/services/fcm_notifications_service.rb
+++ app/services/fcm_notifications_service.rb
@@ -1,7 +1,6 @@
 class FcmNotificationsService
   require 'fcm'
   def self.push_notification(notification)
-    @conversation = Conversation.where(sender_id: notification.send_by_id, recipient_id: notification.user_id).or(Conversation.where(sender_id: notification.user_id, recipient_id: notification.send_by_id))
     data = {
       subject: notification.subject,
       body: notification.body,
@@ -10,7 +9,6 @@ class FcmNotificationsService
       send_by_id: notification.send_by_id,
       send_to_id: notification.user_id,
       booking_sender_id: notification.booking_sender_id,
-      conversation_id: @conversation.first.id,
       sender_profile_image: notification.send_by.profile_image.attached? ? notification.send_by.profile_image.blob.url : ''
     }
     fcm_client = FCM.new(ENV['FIREBASE_SECRET_KEY']) # set your FCM_SERVER_KEY
--- app/views/api/v1/notifications/get_notifications.json.jbuilder
+++ app/views/api/v1/notifications/get_notifications.json.jbuilder
@@ -1,4 +1,5 @@
 json.notification @notifications.each do |notification|
   json.notification notification
   json.send_by_profile_image notification.send_by.profile_image.attached? ? notification.send_by.profile_image.blob.url : ''
+  json.conversation Conversation.where(sender_id: notification.send_by_id, recipient_id: notification.user_id).or(Conversation.where(sender_id: notification.user_id, recipient_id: notification.send_by_id))
 end
